e3078f2c07ba11f6412a4cd8c37428b598521df1
drgnman <drgnman@ht.sfc.keio.ac.jp>
2020-06-11 16:38:29 +0900

    軽微な処理修正

--- src/main/java/com/drgnman/management_server_gradle/controller/TopController.java
+++ src/main/java/com/drgnman/management_server_gradle/controller/TopController.java
@@ -18,19 +18,20 @@ public class TopController {
         CommonSoxProcess soxproc = new CommonSoxProcess();
 
         try {
+            // デバッグ用に使って良い。全部完了したらここの関数は最後に消しておくこと
             // region Subscribeテスト
             // subscriber.CommonSoxSubscribe("ssm-soxfire.ht.sfc.keio.ac.jp","ssmuser",
             //         "ssm2020", "probeDevice");
             // endregion
 
             // region デバイス名一覧取得テスト
-            SoxConnection con = soxproc.CreateSoxConnection("ssm-soxfire.ht.sfc.keio.ac.jp");
-            List<String> nodeList = con.getAllSensorList();
+            // SoxConnection con = soxproc.CreateSoxConnection("ssm-soxfire.ht.sfc.keio.ac.jp");
+            // List<String> nodeList = con.getAllSensorList();
 
-            for(String node:nodeList){
-                System.out.println(node);
-            }
-            System.out.println(nodeList.size());
+            // for(String node:nodeList){
+            //     System.out.println(node);
+            // }
+            // System.out.println(nodeList.size());
             // endregion
 
         } catch (Exception e) {
