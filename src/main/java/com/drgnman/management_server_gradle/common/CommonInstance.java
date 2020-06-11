05dd02e900e1543647b5a3436248ac8f8c5d4351
drgnman <drgnman@ht.sfc.keio.ac.jp>
2020-06-11 16:32:22 +0900

    共通で使う文字列を追加(Success, Error)

--- src/main/java/com/drgnman/management_server_gradle/common/CommonInstance.java
+++ src/main/java/com/drgnman/management_server_gradle/common/CommonInstance.java
@@ -1,4 +1,6 @@
 package com.drgnman.management_server_gradle.common;
 
 public class CommonInstance {
+    public final static String successMsg = "Success";
+    public final static String errorMsg = "Error";
 }
