d4c039abef3cf79e70e57fefe093bb8db16220f0
drgnman <drgnman@ht.sfc.keio.ac.jp>
2020-06-11 16:31:50 +0900

    SOX Fire連携のテストコードを追加

--- src/main/java/com/drgnman/management_server_gradle/controller/TopController.java
+++ src/main/java/com/drgnman/management_server_gradle/controller/TopController.java
@@ -1,14 +1,41 @@
 package com.drgnman.management_server_gradle.controller;
 
+import com.drgnman.management_server_gradle.common.CommonSoxProcess;
+import com.drgnman.management_server_gradle.common.CommonSoxSubscribe;
+import jp.ac.keio.sfc.ht.sox.soxlib.SoxConnection;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 public class TopController {
     @RequestMapping("/")
     @ResponseBody
     public String index() {
+        // CommonSoxSubscribe subscriber= new CommonSoxSubscribe();
+        CommonSoxProcess soxproc = new CommonSoxProcess();
+
+        try {
+            // region Subscribeテスト
+            // subscriber.CommonSoxSubscribe("ssm-soxfire.ht.sfc.keio.ac.jp","ssmuser",
+            //         "ssm2020", "probeDevice");
+            // endregion
+
+            // region デバイス名一覧取得テスト
+            SoxConnection con = soxproc.CreateSoxConnection("ssm-soxfire.ht.sfc.keio.ac.jp");
+            List<String> nodeList = con.getAllSensorList();
+
+            for(String node:nodeList){
+                System.out.println(node);
+            }
+            System.out.println(nodeList.size());
+            // endregion
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return "Hello World!";
     }
 }
