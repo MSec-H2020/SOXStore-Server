768f578a5811b5aba7efb03f5fbb2539a97dc609
drgnman <drgnman@ht.sfc.keio.ac.jp>
2020-06-15 12:56:32 +0900

    メソッドの引数をSoxConnectionに変更

--- src/main/java/com/drgnman/management_server_gradle/common/CommonSoxSubscribe.java
+++ src/main/java/com/drgnman/management_server_gradle/common/CommonSoxSubscribe.java
@@ -10,22 +10,12 @@ import jp.ac.keio.sfc.ht.sox.soxlib.event.SoxEventListener;
 import java.util.List;
 
 public class CommonSoxSubscribe implements SoxEventListener {
-   public void CommonSoxSubscribe(String url, String user_name,
-           String password, String device_name) throws Exception {
-
-      CommonSoxProcess commonSoxProc = new CommonSoxProcess();
-
-      // region Subscribeの一連処理
-      SoxConnection con = commonSoxProc.CreateSoxConnection(url, user_name, password);
-      SoxDevice subscribeDevice = new SoxDevice(con, device_name);
-
-      // Deviceのメタ情報を取得する (今回は使わないかも?)
-      // deviceInfoオブジェクト内にそれぞれのpropertyが存在する
-      // Device deviceInfo = subscribeDevice.getDevice();
-
-       subscribeDevice.subscribe();
-       subscribeDevice.addSoxEventListener(this);
-      // endregion
+   public void CommonSoxSubscribe(SoxConnection con, String device_name) throws Exception {
+        // region Subscribeの一連処理
+        SoxDevice subscribeDevice = new SoxDevice(con, device_name);
+        subscribeDevice.subscribe();
+        subscribeDevice.addSoxEventListener(this);
+        // endregion
    }
 
    @Override
