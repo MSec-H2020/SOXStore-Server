7dfa3b3324210eec8b8fca74e198f5900a476907
drgnman <drgnman@ht.sfc.keio.ac.jp>
2020-06-17 14:20:13 +0900

    DataSearchByTopicIdクエリを追加

--- src/main/java/com/drgnman/management_server_gradle/Repository/DataRepository.java
+++ src/main/java/com/drgnman/management_server_gradle/Repository/DataRepository.java
@@ -1,9 +1,16 @@
 package com.drgnman.management_server_gradle.Repository;
 
 import com.drgnman.management_server_gradle.Entity.Data;
+import com.drgnman.management_server_gradle.Entity.Topic;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface DataRepository extends JpaRepository<Data, Integer> {
+    @Query(value = "SELECT * FROM TOPIC WHERE TOPIC_ID = :topic_id", nativeQuery = true)
+    List<Topic> DataSearchByTopicId(@Param("topic_id") String topic_id);
 }
