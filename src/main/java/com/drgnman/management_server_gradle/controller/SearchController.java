b0b152b2ac140b1a79fda56aa58990f66fe4c0f7
drgnman <drgnman@ht.sfc.keio.ac.jp>
2020-06-22 18:16:14 +0900

    ComplexSearchの枠部分は作った。後はサービス部分のメイン処理だけ

--- src/main/java/com/drgnman/management_server_gradle/controller/SearchController.java
+++ src/main/java/com/drgnman/management_server_gradle/controller/SearchController.java
@@ -1,5 +1,7 @@
 package com.drgnman.management_server_gradle.controller;
 
+import com.drgnman.management_server_gradle.dto.DataDTO;
+import com.drgnman.management_server_gradle.dto.SearchDTO;
 import com.drgnman.management_server_gradle.dto.TopicDTO;
 import com.drgnman.management_server_gradle.service.SearchService;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -19,13 +21,13 @@ public class SearchController {
     SearchService searchService;
     @RequestMapping("/search")
     @ResponseBody
-
-    public String search(@RequestParam(value = "topicId", defaultValue = "") String topicId,
-                           @RequestParam(value = "category", defaultValue = "") String category,
-                           @RequestParam(value = "lat", defaultValue = "") String lat,
-                           @RequestParam(value = "lng", defaultValue = "") String lng,
-                           @RequestParam(value = "distance", defaultValue = "") String distance,
-                           @RequestParam(value = "mode", defaultValue ="simple") String mode) throws JsonProcessingException {
+    public String search(
+            @RequestParam(value = "topicId", defaultValue = "") String topicId,
+            @RequestParam(value = "category", defaultValue = "") String category,
+            @RequestParam(value = "lat", defaultValue = "") String lat,
+            @RequestParam(value = "lng", defaultValue = "") String lng,
+            @RequestParam(value = "distance", defaultValue = "") String distance,
+            @RequestParam(value = "mode", defaultValue ="simple") String mode) throws JsonProcessingException {
         // TOPIC DTOオブジェクトの生成
         TopicDTO searchObj = new TopicDTO();
 
@@ -55,6 +57,47 @@ public class SearchController {
             topicMap.put("testData", resultList);
             json = mapper.writeValueAsString(topicMap);
         }
+
+        return json;
+        // return result;
+    }
+
+    @RequestMapping("/search/complex")
+    @ResponseBody
+    public String complexSearch(
+            // @RequestParam(value = "topicId", defaultValue = "") String topicId,
+            // @RequestParam(value = "category", defaultValue = "") String category,
+            @RequestParam(value = "lat", defaultValue = "") String lat,
+            @RequestParam(value = "lng", defaultValue = "") String lng,
+            @RequestParam(value = "dest_lat", defaultValue = "") String dest_lat,
+            @RequestParam(value = "dest_lng", defaultValue = "") String dest_lng,
+            @RequestParam(value = "expect_time", defaultValue = "") String expect_time) throws JsonProcessingException {
+            // @RequestParam(value = "distance", defaultValue = "") String distance,
+            // @RequestParam(value = "mode", defaultValue ="simple") String mode) throws JsonProcessingException {
+
+        // TOPIC DTOオブジェクトの生成
+        SearchDTO searchObj = new SearchDTO();
+
+        // 戻り値格納用の変数
+        List<DataDTO> resultList;
+        String json;
+
+        // RequestParamの設定
+        // if (!("".equals(topicId))) searchObj.setTopic_id(topicId);
+        // searchObj.setCategory(category);
+        if (!("".equals(lat))) searchObj.setLocation_lat(Double.parseDouble(lat));
+        if (!("".equals(lng))) searchObj.setLocation_lng(Double.parseDouble(lng));
+        if (!("".equals(dest_lat))) searchObj.setDestination_lat(Double.parseDouble(dest_lat));
+        if (!("".equals(dest_lng))) searchObj.setDestination_lat(Double.parseDouble(dest_lng));
+        if (!("".equals(expect_time))) searchObj.setExpect_time(Integer.parseInt(expect_time));
+        // if (!("".equals((distance)))) searchObj.setCover_distance(Double.parseDouble(distance));
+
+        ObjectMapper mapper = new ObjectMapper();
+        // mode="simple"の場合, simpleSearch mode="discover"の場合、distanceSearch
+        resultList = searchService.complexSearch(searchObj);
+        Map<String, List<DataDTO>> topicMap = new HashMap<>();
+        topicMap.put("testData", resultList);
+        json = mapper.writeValueAsString(topicMap);
 
         return json;
         // return result;
