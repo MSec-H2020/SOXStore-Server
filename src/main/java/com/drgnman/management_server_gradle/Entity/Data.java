69094e59bc1e00d049cb64a947ad0b8dfba8096e
drgnman <drgnman@ht.sfc.keio.ac.jp>
2020-06-22 10:38:58 +0900

    cover_distanceを追加

--- src/main/java/com/drgnman/management_server_gradle/Entity/Data.java
+++ src/main/java/com/drgnman/management_server_gradle/Entity/Data.java
@@ -11,11 +11,13 @@ public class Data {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;                    // Data.No (Auto Incrementする)
     private String topic_id;           // トピック名(Topicのtopic_idと=になる)
+    private String data_id;            // データ名(SOXFire上でいう各Transducer名, ない場合はtopic_id=data_idとすること)
     private double location_lat;       // 位置情報(緯度)
     private double location_lng;       // 位置情報(経度)
+    private double cover_distance;      // 検索範囲
     private String value;              // データ本体
-    private int pub_timestamp;      // publishされた時間
-    private int timestamp;          // データが追加された時間
+    private String pub_timestamp;      // publishされた時間
+    private String timestamp;          // データが追加された時間
 
     public int getId() {
         return id;
@@ -27,6 +29,16 @@ public class Data {
     public void setTopic_id(String topic_id) {
         this.topic_id = topic_id;
     }
+
+    public String getData_id() {
+        return data_id;
+    }
+    public void setData_id(String data_id) {
+        this.data_id = data_id;
+    }
+
+    public double getCover_distance() { return cover_distance; }
+    public void setCover_distance(double cover_distance) { this.cover_distance = cover_distance; }
 
     public double getLocation_lat() {
         return location_lat;
@@ -49,17 +61,14 @@ public class Data {
         this.value = value;
     }
 
-    public int getPub_timestamp() {
+    public String getPub_timestamp() {
         return pub_timestamp;
     }
-    public void setPub_timestamp(int pub_timestamp) {
+    public void setPub_timestamp(String pub_timestamp) {
         this.pub_timestamp = pub_timestamp;
     }
 
-    public int getTimestamp() {
+    public String getTimestamp() {
         return timestamp;
-    }
-    public void setTimestamp(int timestamp) {
-        this.timestamp = timestamp;
     }
 }
