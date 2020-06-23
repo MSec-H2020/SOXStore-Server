38c9cd495169e4e546a311633b39c5eed230d46e
drgnman <drgnman@ht.sfc.keio.ac.jp>
2020-06-23 11:09:19 +0900

    regionの追加

--- src/main/java/com/drgnman/management_server_gradle/Repository/TopicRepository.java
+++ src/main/java/com/drgnman/management_server_gradle/Repository/TopicRepository.java
@@ -10,28 +10,55 @@ import java.util.List;
 
 @Repository
 public interface TopicRepository extends JpaRepository<Topic, Integer> {
+    // region 単純検索
+    // 単純検索(Topic ID検索)
+    @Query(value = "SELECT * FROM TOPIC WHERE TOPIC_ID = :topic_id", nativeQuery = true)
+    Topic TopicSearchByTopicId(@Param("topic_id") String topic_id);
+    // endregion
+
+    // region 現在地周辺のデータを検索
+    // 現在地の周辺イベントを取得するため、経過予測時間は適用しない
     @Query(value = "SELECT TOPIC_ID, CATEGORY, LOCATION_LAT, LOCATION_LNG," +
             " (" +
             "6371 * acos(" +
-            "cos(radians(35.6804067))" +
+            "cos(radians(:lat))" +
             "* cos(radians(LOCATION_LAT))" +
-            "* cos(radians(LOCATION_LNG) - radians(139.7550152))" +
-            "+ sin(radians(35.6804067))" +
+            "* cos(radians(LOCATION_LNG) - radians(:lng))" +
+            "+ sin(radians(:lat))" +
             "* sin(radians(LOCATION_LAT))" +
             ")" +
             ") AS TEMP_DISTANCE" +
-            " FROM TOPIC HAVING TEMP_DISTANCE <= 3.0", nativeQuery = true) // SQL
+            " FROM TOPIC HAVING TEMP_DISTANCE <= :distance", nativeQuery = true) // SQL
     List<Object> TopicSearchForDistance(
               @Param("lat") double lat,
               @Param("lng") double lng,
               @Param("distance") double distance
     );
+    // endregion
 
-    // 単純検索(Topic ID検索)
-    @Query(value = "SELECT * FROM TOPIC WHERE TOPIC_ID = :topic_id", nativeQuery = true)
-    Topic TopicSearchByTopicId(@Param("topic_id") String topic_id);
+    // region 目的地周辺のデータを検索
+    // 現在地の座標もしくは目的地の座標と経過予測時間が大きなものを取得するクエリ(検索範囲を追加したversion.)
+    @Query(value = "SELECT TOPIC_ID, " +
+            "6371 * acos(" +
+            "cos(radians(:dest_lat))" +
+            "* cos(radians(LOCATION_LAT))" +
+            "* cos(radians(LOCATION_LNG) - radians(:dest_lng))" +
+            "+ sin(radians(:dest_lat))" +
+            "* sin(radians(LOCATION_LAT))" +
+            ")" +
+            ") AS TEMP_DISTANCE " +
+            " FROM TOPIC WHERE LIFETIME = :expect_time HAVING CURRENT_DISTANCE <= :distance", nativeQuery = true) // SQL
+    List<Object> TopicSearchForDestinationAndRange(
+            @Param("dest_lat") double dest_lat,
+            @Param("dest_lng") double dest_lng,
+            @Param("distance") double distance,
+            @Param("expect_time") int expect_time
+    );
+    // endregion
 
-    @Query(value = "SELECT * FROM TOPIC WHERE (LOCATION_LAT = :current_lat AND LOCATION_LNG = :current_lng) " +
+    // region 複合検索1(使わないかも)
+    // 現在地の座標もしくは目的地の座標と経過予測時間が大きなものを取得するクエリ
+    @Query(value = "SELECT TOPIC_ID FROM TOPIC WHERE (LOCATION_LAT = :current_lat AND LOCATION_LNG = :current_lng) " +
             "OR (LOCATION_LAT = :dest_lat AND LOCATION_LNG = :dest_lng AND LIFETIME <= :expect_time)", nativeQuery = true)
     List<Object> TopicSearchForComplexLocation(
             @Param("current_lat") double current_lat,
@@ -40,6 +67,9 @@ public interface TopicRepository extends JpaRepository<Topic, Integer> {
             @Param("dest_lng") double dest_lng,
             @Param("expect_time") int expect_time
     );
+    // endregion
+
+    // region 動かなかったupdateクエリ
     // うまく動かないため、saveメソッドで代用
     // // LifeTimeの更新(Topic ID指定による)
     // @Query(value = "UPDATE TOPIC SET LIFETIME = :lifetime  WHERE TOPIC_ID = :topic_id", nativeQuery = true)
@@ -52,5 +82,7 @@ public interface TopicRepository extends JpaRepository<Topic, Integer> {
     //                            @Param("location_lng") double location_lng,
     //                            @Param("cover_distance") double cover_distance,
     //                            @Param("lifetime") int lifetime);
+    // endregion
+
 }
 
