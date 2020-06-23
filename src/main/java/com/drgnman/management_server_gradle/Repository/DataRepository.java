198b46bf3dd80f3af2f90647788bd494368033ef
drgnman <drgnman@ht.sfc.keio.ac.jp>
2020-06-23 10:05:30 +0900

    TOPIC_IDに紐づくデータの中から最新の一件の取得を行うクエリの追加

--- src/main/java/com/drgnman/management_server_gradle/Repository/DataRepository.java
+++ src/main/java/com/drgnman/management_server_gradle/Repository/DataRepository.java
@@ -11,8 +11,12 @@ import java.util.List;
 @Repository
 public interface DataRepository extends JpaRepository<Data, Integer> {
     // TOPIC_IDﾆ紐づく全レコードの取得
-    @Query(value = "SELECT * FROM TOPIC WHERE TOPIC_ID = :topic_id", nativeQuery = true)
+    @Query(value = "SELECT * FROM DATA WHERE TOPIC_ID = :topic_id", nativeQuery = true)
     List<Data> DataSearchByTopicId(@Param("topic_id") String topic_id);
+
+    // TOPIC_IDﾆ紐づくデータの中で最新の1件の取得
+    @Query(value = "SELECT * FROM DATA WHERE TOPIC_ID = :topic_id ORDER BY ID DESC LIMIT 1", nativeQuery = true)
+    Data DataSearchByTopicIdLimit1(@Param("topic_id") String topic_id);
 
     // TOPIC_IDに紐づく全レコードのタイムスタンプデータだけ全取得
     @Query(value = "SELECT ID, UNIX_TIMESTAMP(PUB_TIMESTAMP), UNIX_TIMESTAMP(TIMESTAMP) FROM DATA WHERE TOPIC_ID = :topic_id ORDER BY ID DESC", nativeQuery = true)
