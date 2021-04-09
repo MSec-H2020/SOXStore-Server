cd8d34db51afcb31ca99e222fdff31a09940b4b3
drgnman <drgnman@ht.sfc.keio.ac.jp>
2021-04-09 18:23:54 +0900

    publish機能を追加

--- src/main/java/com/drgnman/management_server_gradle/common/CommonSoxProcess.java
+++ src/main/java/com/drgnman/management_server_gradle/common/CommonSoxProcess.java
@@ -1,8 +1,10 @@
 package com.drgnman.management_server_gradle.common;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
+import com.drgnman.management_server_gradle.dto.PublishDTO;
 import jp.ac.keio.sfc.ht.sox.soxlib.SoxConnection;
 import org.jivesoftware.smack.SmackException;
 import org.jivesoftware.smack.XMPPException;
@@ -43,4 +45,49 @@ public class CommonSoxProcess {
         return deviceInfo;
     }
     // endregion
+
+    public String Publish(SoxConnection con, PublishDTO publishDto) throws Exception {
+        SoxDevice soxDevice = new SoxDevice(con, publishDto.getTheme());
+        List<TransducerValue> valueList = new ArrayList<TransducerValue>();
+
+        TransducerValue username = new TransducerValue();
+        username.setId("UserName");
+        username.setRawValue(publishDto.getUsername());
+        username.setTypedValue(publishDto.getUsername());
+        username.setCurrentTimestamp();
+        valueList.add(username);
+
+        TransducerValue latitude = new TransducerValue();
+        latitude.setId("Latitude");
+        latitude.setRawValue(publishDto.getLatitude());
+        latitude.setTypedValue(publishDto.getLatitude());
+        latitude.setCurrentTimestamp();
+        valueList.add(latitude);
+
+        TransducerValue longitude = new TransducerValue();
+        longitude.setId("Longitude");
+        longitude.setRawValue(publishDto.getLongitude());
+        longitude.setTypedValue(publishDto.getLongitude());
+        longitude.setCurrentTimestamp();
+        valueList.add(longitude);
+
+
+        TransducerValue smiledegree = new TransducerValue();
+        smiledegree.setId("SmileDegree");
+        smiledegree.setRawValue(publishDto.getSmiledegree());
+        smiledegree.setTypedValue(publishDto.getSmiledegree());
+        smiledegree.setCurrentTimestamp();
+        valueList.add(smiledegree);
+
+        TransducerValue imagephoto = new TransducerValue();
+        imagephoto.setId("ImagePhoto");
+        imagephoto.setRawValue(publishDto.getImagephoto());
+        imagephoto.setTypedValue(publishDto.getImagephoto());
+        imagephoto.setCurrentTimestamp();
+        valueList.add(imagephoto);
+
+        soxDevice.publishValues(valueList);
+
+        return "Publish to " + publishDto.getTheme() + " Complete";
+    }
 }
