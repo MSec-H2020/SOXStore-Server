85232bf54c57a47ebf82628758216ef4278e0c15
drgnman <drgnman@ht.sfc.keio.ac.jp>
2021-07-05 16:05:08 +0900

    複数のSOXFire serverへの接続を管理できるように修正+前方一致検索の追加

--- src/main/java/com/drgnman/management_server_gradle/service/SoxSyncService.java
+++ src/main/java/com/drgnman/management_server_gradle/service/SoxSyncService.java
@@ -1,7 +1,9 @@
 package com.drgnman.management_server_gradle.service;
 
+import com.drgnman.management_server_gradle.Entity.SOX_SERVER_INFO;
 import com.drgnman.management_server_gradle.Entity.Topic;
 import com.drgnman.management_server_gradle.Repository.DataRepository;
+import com.drgnman.management_server_gradle.Repository.SoxServerInfoRepository;
 import com.drgnman.management_server_gradle.Repository.TopicRepository;
 import com.drgnman.management_server_gradle.common.CommonDBConnector;
 import com.drgnman.management_server_gradle.common.CommonSoxProcess;
@@ -10,6 +12,7 @@ import com.drgnman.management_server_gradle.dto.DeviceDTO;
 import jp.ac.keio.sfc.ht.sox.protocol.Device;
 import jp.ac.keio.sfc.ht.sox.protocol.Transducer;
 import jp.ac.keio.sfc.ht.sox.soxlib.SoxConnection;
+import org.apache.catalina.util.ServerInfo;
 import org.jivesoftware.smack.SmackException;
 import org.jivesoftware.smack.XMPPException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +22,7 @@ import org.springframework.stereotype.Service;
 
 import javax.sql.DataSource;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -31,6 +35,8 @@ public class SoxSyncService {
     TopicRepository topicRepository;
     @Autowired
     DataRepository dataRepository;
+    @Autowired
+    SoxServerInfoRepository soxServerInfoRepository;
     // region デバイス名一覧取得テスト
     CommonSoxProcess soxproc = new CommonSoxProcess();
     // CommonDBConnector dbconnector = new CommonDBConnector();
@@ -39,21 +45,51 @@ public class SoxSyncService {
     // String sql = "INSERT INTO topic values(?, ?)";
 
     // public void soxSync(String url, String user, String pass, String node, String db_url, String db_user, String db_pass) throws SQLException {
-    public void soxSync(String url, String user, String pass, List<String> nodeList) throws SQLException {
+    public void soxSync(String url, String user, String pass, List<String> nodeList, String keyword) throws SQLException {
         try {
             SoxConnection con = soxproc.CreateSoxConnection(url, user, pass);
+
+            SOX_SERVER_INFO soxServerInfo = new SOX_SERVER_INFO();
+
+            if (keyword.equals("yes")) {
+                List<String> allDeviceList = con.getAllSensorList();
+                List<String> tmpList = new ArrayList<String>(nodeList);
+                nodeList.clear();
+                for (String node: tmpList){
+                    for(String deviceName: allDeviceList) {
+                       if(deviceName.startsWith(node)) {
+                           nodeList.add(deviceName);
+                       }
+                    }
+                }
+            }
+
+            soxServerInfo.setUrl(url);
+            soxServerInfo.setUser_name(user);
+            soxServerInfo.setPassword(pass);
+            soxServerInfoRepository.save(soxServerInfo);
 
             for(String node : nodeList) {
                 Device device = soxproc.DeviceInfo(con, node);
                 Topic topic = new Topic();
                 topic.setTopic_id(device.getId());
                 topic.setCategory(device.getDeviceType().toString());
+                topic.setServer_name(url);
                 topicRepository.save(topic);
             }
 
-            List<Topic> topicList = topicRepository.findAll();
-            for(Topic tp: topicList) {
-                new CommonSoxSubscribe(con, topicRepository, dataRepository, tp.getTopic_id());
+
+            // List<Topic> topicList = topicRepository.findAll();
+            List<Object> serverList = topicRepository.TopicSearchAndSoxInformation();
+            for(Object tp: serverList) {
+                int length = Array.getLength(tp);
+                List list = new ArrayList();
+                for (int i=0; i < length; i++){
+                   list.add(Array.get(tp, i));
+                }
+                con = soxproc.CreateSoxConnection(list.get(4).toString(), list.get(5).toString(), list.get(6).toString());
+                // new CommonSoxSubscribe(con, topicRepository, dataRepository, tp.getTopic_id());
+                new CommonSoxSubscribe(con, topicRepository, dataRepository, list.get(0).toString());
             }
         } catch (IOException ioException) {
             ioException.printStackTrace();
@@ -76,7 +112,7 @@ public class SoxSyncService {
     // endregion
 
     // region soxGetAllDevices
-    public List<String> soxGetDevices(String url, String user, String pass) throws SQLException {
+    public List<String> soxGetAllDevices(String url, String user, String pass) throws SQLException {
         List<String> nodeList = null;
         try {
             SoxConnection con = soxproc.CreateSoxConnection(url, user, pass);
