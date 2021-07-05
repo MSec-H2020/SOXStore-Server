35418daa472fbd99eb37203e812714b94f7e0c7b
drgnman <drgnman@ht.sfc.keio.ac.jp>
2021-07-05 16:05:44 +0900

    サーバ情報の追加(外部キー)

--- src/main/java/com/drgnman/management_server_gradle/Entity/Topic.java
+++ src/main/java/com/drgnman/management_server_gradle/Entity/Topic.java
@@ -1,15 +1,21 @@
 package com.drgnman.management_server_gradle.Entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
+@IdClass(CompositeKeyTopic.class)
 public class Topic {
     @Id
     private String topic_id;            // トピックID(名称)
     private String category;            // カテゴリ名
+    @Id
+    private String server_name;
+
+    // @ManyToOne
+    // @JoinColumn(name="url")
+    // private SOX_SERVER_INFO soxServerInfo;
+
 
     public String getTopic_id() {
         return topic_id;
@@ -18,10 +24,26 @@ public class Topic {
         this.topic_id = topic_id;
     }
 
+
     public String getCategory() {
         return category;
     }
     public void setCategory(String category) {
         this.category = category;
     }
+
+    public String getServer_name() {
+        return server_name;
+    }
+    public void setServer_name(String server_name) {
+        this.server_name = server_name;
+    }
+
+    // public SOX_SERVER_INFO getSoxServerInfo() {
+    //     return soxServerInfo;
+    // }
+
+    // public void setSoxServerInfo(SOX_SERVER_INFO soxServerInfo) {
+    //     this.soxServerInfo = soxServerInfo;
+    // }
 }
