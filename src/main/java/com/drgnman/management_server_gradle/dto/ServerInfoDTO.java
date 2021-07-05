ed9959f8089710e3fd0a93a3a3f3dc1b4be111aa
drgnman <drgnman@ht.sfc.keio.ac.jp>
2021-07-05 16:04:15 +0900

    接続先のSOXFireの情報を保持しておくためのEntityを追加

---
+++ src/main/java/com/drgnman/management_server_gradle/dto/ServerInfoDTO.java
@@ -0,0 +1,33 @@
+package com.drgnman.management_server_gradle.dto;
+
+import javax.persistence.Id;
+
+public class ServerInfoDTO {
+    private String url;
+    private String user_name;
+    private String password;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUser_name() {
+        return user_name;
+    }
+
+    public void setUser_name(String user_name) {
+        this.user_name = user_name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
